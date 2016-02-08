28d54b4ab9c3ee12bafc188a115ced79d67e0559
Kyle McKee <mckee.kyle@gmail.com>
2016-02-07 19:29:39 -0500

    Added chai, fixed step defs to assert properly

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -1,16 +1,25 @@
-
 var By = require('selenium-webdriver').By;
+var expect = require('chai').expect;
 module.exports = function () {
     this.When(/^I am viewing the sample login page$/, function () {
-        // Write code here that turns the phrase above into concrete actions
         this.browser.get('http://localhost:3000/signup_form.html');
+        // This should not be so complicated.  Want to get to this:
+        // visit(SignupFormPage);
     });
-
-    this.Then(/^a SignUp button should exist$/, function () {
-        console.log(this.browser.isElementPresent(By.id('sign-up')));
+    this.Then(/^a SignUp button should exist$/, function (done) {
+        this.browser.isElementPresent(By.id('sign-up')).then((val) => {
+            expect(val).to.equal(true);
+            done();
+        });
+        // This should not be so complicated.  Want to get to this:
+        // expect(on(SignupFormPage).signUp.visible).to.equal(true);
     });
-
-    this.Then(/^a Foo button should not exist$/, function () {
-        console.log(this.browser.isElementPresent(By.id('sign-up')));
+    this.Then(/^a Foo button should not exist$/, function (done) {
+        this.browser.isElementPresent(By.id('foo')).then((val) => {
+            expect(val).to.equal(false);
+            done();
+            // This should not be so complicated.  Want to get to this:
+            // expect(on(SignupFormPage).foo.visible).to.equal(true);
+        });
     });
 };
