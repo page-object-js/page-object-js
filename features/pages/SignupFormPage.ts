f505a311b779b2751fbf7ac633a50e53642e1a16
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-02-10 23:42:26 -0500

    added a disabled button to the html as well as the class

--- features/pages/SignupFormPage.ts
+++ features/pages/SignupFormPage.ts
@@ -4,4 +4,5 @@ export class SignupFormPage {
     public signUp:  Button = new Button({id: 'sign-up'});
     public foo:     Button = new Button({id: 'foo'});
     public submitCount: Span = new Span({id: 'submit-count'});
+    public deleteDatabase: Button = new Button({ id: 'delete-database' });
 }
