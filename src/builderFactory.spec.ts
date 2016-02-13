2dcb110ba5d3f476816ff50ade43aaee3442ac71
Kyle McKee <mckee.kyle@gmail.com>
2016-02-13 02:29:00 -0500

    Wip.  pushed tests a little further

--- src/builderFactory.spec.ts
+++ src/builderFactory.spec.ts
@@ -1,27 +1,28 @@
 import * as test from  "tape";
 import {on} from "./builderFactory";
-import {Promise} from 'es6-promise';
+import {Promise} from "es6-promise";
 
 abstract class Page {
     public then(): Promise<any> {
-        return Promise.resolve(null)
+        return Promise.resolve(null);
     }
 }
 
-import {Button} from './button';
+import {Button} from "./button";
 class SamplePage extends Page {
-    public submit: Button = new Button({id: 'submit'});
+    public submit: Button = new Button({id: "submit"});
 }
 
 test("on()",  function (t: test.Test):void {
     t.test("returns an instance of the page", function (t: test.Test):void {
-        t.plan(1);
+        t.plan(2);
         let result:SamplePage = on(SamplePage);
         t.notEqual(result, undefined);
         t.notEqual(result, null);
     });
     t.test("actions return the page instance", function (t: test.Test):void {
-        t.plan(0);  // <== The fact that this test compiles is good enough.
+        t.plan(1);
         let result:SamplePage = on(SamplePage).submit.visible();
+        t.true(result instanceof SamplePage);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // This is the main module.
 // Whatever this module exports is what the client will get when requiring this
 // node package:
-export {on} from "./BuilderFactory";
+export {on} from "./builderFactory";
 export {getBrowser, setBrowser} from "./browser";
 export {Button} from "./button";
 export {Span} from "./span";
