a327558143d1e145ac94f432140114762a64bb0c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-28 22:49:42 -0400

    started a base element class

---
+++ src/base.ts
@@ -0,0 +1,29 @@
+import * as selwd from "selenium-webdriver";
+const by: typeof selwd.By = selwd.By;
+
+import {getBrowser} from "./index";
+import Locator from "./locator";
+
+export class BaseElement {
+    private _locator: Locator;
+
+    constructor(locator: Locator) {
+        this._locator = locator;
+    }
+
+    click(): selwd.promise.Promise<void> {
+        return getBrowser().findElement(by.id(this._locator.id)).click();
+    }
+
+    get innerHtml(): selwd.promise.Promise<string> {
+        return getBrowser().findElement(by.id(this._locator.id)).getInnerHtml();
+    }
+
+    get outerHtml(): selwd.promise.Promise<string> {
+        return getBrowser().findElement(by.id(this._locator.id)).getOuterHtml();
+    }
+
+    get text(): selwd.promise.Promise<string> {
+        return getBrowser().findElement(by.id(this._locator.id)).getText();
+    }
+}
