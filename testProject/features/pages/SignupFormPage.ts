926893b636dc379b6d4fab310218d30cf79ae166
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-05-11 21:52:49 -0400

    use locators to find elements in the page object

--- testProject/features/pages/SignupFormPage.ts
+++ testProject/features/pages/SignupFormPage.ts
@@ -1,8 +1,8 @@
-import {Button, Span} from 'page-object-js';
+import {Button, Span, IdLocator} from 'page-object-js';
 
 export class SignupFormPage {
-    public signUp:  Button = new Button({id: 'sign-up'});
-    public foo:     Button = new Button({id: 'foo'});
-    public submitCount: Span = new Span({id: 'submit-count'});
-    public deleteDatabase: Button = new Button({ id: 'delete-database' });
+    public signUp: Button = new Button(new IdLocator('sign-up'));
+    public foo: Button = new Button(new IdLocator('foo'));
+    public submitCount: Span = new Span(new IdLocator('submit-count'));
+    public deleteDatabase: Button = new Button(new IdLocator('delete-database'));
 }
